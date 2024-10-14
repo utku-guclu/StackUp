4140a551b7987b70f8c9942b910258f64cc43092
utku-guclu <utku_guclu@hotmail.com>
2024-10-14 10:09:41 +0300

    feat: Add session storage handling for authentication

--- packages/frontend/src/services/auth/authSlice.ts
+++ packages/frontend/src/services/auth/authSlice.ts
@@ -1,86 +1,105 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { createSlice } from "@reduxjs/toolkit"; // Correct the import
 import type {
-	UserResponse,
-	LoginRequest,
-	LogOutResponse,
-	AuthState,
-	RegisterResponse,
-	RegisterRequest,
+  UserResponse,
+  LoginRequest,
+  LogOutResponse,
+  AuthState,
+  RegisterResponse,
+  RegisterRequest,
 } from "./types";
 import type { RootState } from "../../store";
 
 // Create the API service
 export const authBlogApi = createApi({
-	baseQuery: fetchBaseQuery({
-    	// Replace your address here if needed i.e. your forwarded address from a cloud environment
-    	baseUrl: "http://127.0.0.1:4040/api/",
-    	prepareHeaders: (headers, { getState }) => {
-        	const token = (getState() as RootState).auth.token;
-        	if (token) {
-            	headers.set("Authorization", `Bearer ${token}`);
-        	}
-        	return headers;
-    	},
-    	credentials: "include", // Include credentials for CORS
-	}),
-	endpoints: (builder) => ({
-    	login: builder.mutation<UserResponse, LoginRequest>({
-        	query: (credentials) => ({
-            	url: "auth/login",
-            	method: "POST",
-            	body: credentials,
-        	}),
-    	}),
-    	logout: builder.mutation<LogOutResponse, void>({
-        	query: () => ({
-            	url: "auth/logout",
-            	method: "POST",
-        	}),
-    	}),
-    	register: builder.mutation<RegisterResponse, RegisterRequest>({
-        	query: (info) => ({
-            	url: "auth/register",
-            	method: "POST",
-            	body: info,
-        	}),
-    	}),
-	}),
+  baseQuery: fetchBaseQuery({
+    // Replace your address here if needed i.e. your forwarded address from a cloud environment
+    baseUrl: "http://127.0.0.1:4040/api/",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+    credentials: "include", // Include credentials for CORS
+  }),
+  endpoints: (builder) => ({
+    login: builder.mutation<UserResponse, LoginRequest>({
+      query: (credentials) => ({
+        url: "auth/login",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+    logout: builder.mutation<LogOutResponse, void>({
+      query: () => ({
+        url: "auth/logout",
+        method: "POST",
+      }),
+    }),
+    register: builder.mutation<RegisterResponse, RegisterRequest>({
+      query: (info) => ({
+        url: "auth/register",
+        method: "POST",
+        body: info,
+      }),
+    }),
+  }),
 });
 
 // Create the auth slice
 const authSlice = createSlice({
-	name: "auth",
-	initialState: {
-    	user: null,
-    	token: null,
-	} as AuthState,
-	reducers: {},
-	extraReducers: (builder) => {
-    	builder.addMatcher(
-        	authBlogApi.endpoints.login.matchFulfilled,
-        	(state, { payload }) => {
-            	state.token = payload.token;
-            	state.user = {
-                	id: payload.userId,
-                	username: payload.username,
-                	email: payload.email,
-                	role: payload.role,
-            	};
-        	}
-    	);
-    	builder.addMatcher(authBlogApi.endpoints.logout.matchFulfilled, (state) => {
-        	state.token = null;
-        	state.user = null;
-    	});
-	},
+  name: "auth",
+  initialState: {
+    user: null,
+    token: null,
+  } as AuthState,
+  reducers: {
+    refreshAuthentication: (state) => {
+      const isAuthenticated = sessionStorage.getItem("isAuthenticated");
+      if (isAuthenticated === "true") {
+        const userSession = sessionStorage.getItem("user");
+        const response: UserResponse = JSON.parse(
+          userSession as string,
+        ) as UserResponse;
+        state.token = response.token;
+        state.user = {
+          username: response.username,
+          id: response.userId,
+          email: response.email,
+          role: response.role,
+        };
+      }
+      return state;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      authBlogApi.endpoints.login.matchFulfilled,
+      (state, { payload }) => {
+        state.token = payload.token;
+        state.user = {
+          id: payload.userId,
+          username: payload.username,
+          email: payload.email,
+          role: payload.role,
+        };
+      },
+    );
+    builder.addMatcher(authBlogApi.endpoints.logout.matchFulfilled, (state) => {
+      state.token = null;
+      state.user = null;
+      sessionStorage.removeItem("isAuthenticated");
+      sessionStorage.removeItem("user");
+      return state;
+    });
+  },
 });
 
 export default authSlice.reducer;
 
+export const { refreshAuthentication } = authSlice.actions;
 // Export the hooks from the API
-export const {
-	useLoginMutation,
-	useLogoutMutation,
-	useRegisterMutation,
-} = authBlogApi;
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation } =
+  authBlogApi;
