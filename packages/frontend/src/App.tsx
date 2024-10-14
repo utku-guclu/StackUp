de4beae3302b8d7e8935fb73e91f737cd9382f1d
utku-guclu <utku_guclu@hotmail.com>
2024-10-14 10:11:29 +0300

    feat: Implement user session handling in the App component

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -11,68 +11,90 @@ import NotFound from "./pages/404";
 import Register from "./pages/auth/Register";
 import "./App.css";
 
+import type { AuthState, UserResponse } from "./services/auth/types";
+
 const App = () => {
-	const authState = useAppSelector((state) => state.auth);
-	const isAuthenticated = authState.user !== null && authState.token !== null;
+  let authState: AuthState = {
+    user: null,
+    token: null,
+  };
+  const { user, token } = useAppSelector((state) => state.auth);
+  const userSession = sessionStorage.getItem("user");
+  const response: UserResponse = userSession ? JSON.parse(userSession) : null;
+  if (
+    sessionStorage.getItem("isAuthenticated") === "true" &&
+    response !== null
+  ) {
+    authState = {
+      user:
+        {
+          username: response.username,
+          id: response.userId,
+          email: response.email,
+          role: response.role,
+        } ?? user,
+      token: response.token ?? token,
+    };
+  }
+  const isAuthenticated = authState.user !== null && authState.token !== null;
 
-	const router = createBrowserRouter([
-    	{
-        	path: "/",
-        	element: (
-            	<Login authState={authState} isAuthenticated={isAuthenticated} />
-        	),
-        	children: [
-            	{
-                	path: "register",
-                	element: (
-                    	<Register isAuthenticated={isAuthenticated} />
-                	),
-            	},
-        	],
-    	},
-    	{
-        	path: "/post/create/",
-        	element: (
-            	<CreatePost isAuthenticated={isAuthenticated} authState={authState} />
-        	),
-    	},
-    	{
-        	path: "/posts/",
-        	element: (
-            	<Posts isAuthenticated={isAuthenticated} authState={authState} />
-        	),
-        	children: [
-            	{
-                	path: "",
-                	element: <AllPost />,
-            	},
-            	{
-                	path: "user/:username",
-                	element: <UserSpecificPosts isAuthenticated={isAuthenticated} />,
-                	loader: async ({ params }) => {
-                    	return params.username;
-                	},
-            	},
-            	{
-                	path: "user/:username/post/edit/:postId",
-                	element: <EditPost isAuthenticated={isAuthenticated} />,
-                	loader: ({ params }) => {
-                    	return { username: params.username, postId: params.postId };
-                	},
-            	},
-        	],
-    	},
-    	{
-        	path: "*",
-        	element: <NotFound />,
-    	},
-	]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Login authState={authState} isAuthenticated={isAuthenticated} />
+      ),
+      children: [
+        {
+          path: "register",
+          element: <Register isAuthenticated={isAuthenticated} />,
+        },
+      ],
+    },
+    {
+      path: "/post/create/",
+      element: (
+        <CreatePost isAuthenticated={isAuthenticated} authState={authState} />
+      ),
+    },
+    {
+      path: "/posts/",
+      element: (
+        <Posts isAuthenticated={isAuthenticated} authState={authState} />
+      ),
+      children: [
+        {
+          path: "",
+          element: <AllPost />,
+        },
+        {
+          path: "user/:username",
+          element: <UserSpecificPosts isAuthenticated={isAuthenticated} />,
+          loader: async ({ params }) => {
+            return params.username;
+          },
+        },
+        {
+          path: "user/:username/post/edit/:postId",
+          element: <EditPost isAuthenticated={isAuthenticated} />,
+          loader: ({ params }) => {
+            return { username: params.username, postId: params.postId };
+          },
+        },
+      ],
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
 
-	return (
-    	<div>
-        	<RouterProvider router={router} />
-    	</div>
-	);
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 
 export default App;
+
