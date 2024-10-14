1bf28b55e396ece3a1c9bd0ab8a0a6fa008f272a
utku-guclu <utku_guclu@hotmail.com>
2024-10-14 09:33:56 +0300

    feat: Add new origin to CORS configuration

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -16,41 +16,42 @@ app.use(morgan("dev"));
 app.use(cookieParser(config.TOKEN));
 
 const corsOptions = {
-	// Add your address here i.e. your forwarded address from a cloud environment
-	origin: [
-		"http://127.0.0.1:5173",
-		"http://127.0.0.1:4173",
-		"http://localhost:5173",
-		"http://localhost:4173",
-		"http://[::1]:4173",
-		"http://[::1]:5173",
-	],
-	credentials: true, //included credentials as true
-	preflightContinue: true,
+  // Add your address here i.e. your forwarded address from a cloud environment
+  origin: [
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:4173",
+    "http://127.0.0.1:4040",
+    "http://localhost:5173",
+    "http://localhost:4173",
+    "http://[::1]:4173",
+    "http://[::1]:5173",
+  ],
+  credentials: true, //included credentials as true
+  preflightContinue: true,
 };
 
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-	res.header(
-		"Access-Control-Allow-Methods",
-		"GET, POST, OPTIONS, PUT, PATCH, DELETE",
-	);
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Access-Control-Allow-Origin, X-Requested-With,content-type,Content-Type, Authorization,Authentication,withCredentials, Content-Length, X-Requested-With, Accept, x-access-token,credentials, Origin, X-Content-Type-Options",
-	);
-	res.header(
-		"Access-Control-Expose-Headers",
-		"x-access-token, Authorization, Authentication, withCredentials, credentials, Set-Cookie",
-	);
-	res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Origin, X-Requested-With,content-type,Content-Type, Authorization,Authentication,withCredentials, Content-Length, X-Requested-With, Accept, x-access-token,credentials, Origin, X-Content-Type-Options",
+  );
+  res.header(
+    "Access-Control-Expose-Headers",
+    "x-access-token, Authorization, Authentication, withCredentials, credentials, Set-Cookie",
+  );
+  res.header("Access-Control-Allow-Credentials", true);
 
-	// You might want to hard-code this if necessary.
-	const origin = req.get("origin") || req.get("referer");
-	res.header("Access-Control-Allow-Origin", origin);
+  // You might want to hard-code this if necessary.
+  const origin = req.get("origin") || req.get("referer");
+  res.header("Access-Control-Allow-Origin", origin);
 
-	next();
+  next();
 });
 
 app.use(express.json());
@@ -62,9 +63,9 @@ app.use("/api/auth/", authRoute);
 app.use("/api/posts/", accessControlRoutes);
 
 try {
-	app.listen(PORT, () =>
-		console.log(`Connected and listening on port ${PORT}.`),
-	);
+  app.listen(PORT, () =>
+    console.log(`Connected and listening on port ${PORT}.`),
+  );
 } catch (err) {
-	console.log(`Failed to start the server with error: ${err}`);
+  console.log(`Failed to start the server with error: ${err}`);
 }
